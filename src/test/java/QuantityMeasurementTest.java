a3eb79466ce7df9649201302efc11a490c4d32c0
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-14 02:12:49 +0530

    REFACTORED Test cases to sync with new class structure

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,34 +1,42 @@
-import com.quantityMeasurement.Unit;
+import com.quantityMeasurement.QuantityMeasurementException;
 import com.quantityMeasurement.UnitComparator;
+import com.quantityMeasurement.UnitCreatorFactory;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static com.quantityMeasurement.QuantityMeasurementException.ExceptionType.*;
+import static com.quantityMeasurement.Unit.*;
 
 public class QuantityMeasurementTest {
     UnitComparator unitComparator = new UnitComparator();
 
     @Test
-    public void givenSameUnitFeet_ReturnsTrue() {
-        Unit feet1 = new Unit(0);
-        Unit feet2 = new Unit(0);
-        Assert.assertTrue(this.unitComparator.compare(feet1,feet2));
+    public void givenSameUnitFeet_ReturnsTrue() throws QuantityMeasurementException {
+        UnitCreatorFactory feet1 = new UnitCreatorFactory(0.0, FEET);
+        UnitCreatorFactory feet2 = new UnitCreatorFactory(0.0, FEET);
+        Assert.assertTrue(unitComparator.compare(feet1, feet2));
     }
 
     @Test
     public void givenNullValueWhileComparingUnits_ShouldReturnFalse() {
-        Unit feet1 = new Unit(0);
-        Assert.assertFalse(this.unitComparator.compare(feet1, null));
+        UnitCreatorFactory feet1 = new UnitCreatorFactory(0.0, FEET);
+        try {
+            unitComparator.compare(feet1, null);
+        } catch (QuantityMeasurementException e) {
+            Assert.assertEquals(NO_UNIT_FOUND, e.type);
+        }
     }
 
     @Test
     public void givenSameReferanceOfUnit_ShouldReturnTrue() {
-        Unit feet1 = new Unit(0);
-        Assert.assertEquals(feet1,feet1);
+        UnitCreatorFactory feet = new UnitCreatorFactory(0.0, FEET);
+        Assert.assertEquals(feet, feet);
     }
 
     @Test
     public void givenTwoObjectsOfFeetWithSameType_ShouldReturnTrue() {
-        Unit feet1 = new Unit(0);
-        Unit feet2 = new Unit(1);
+        UnitCreatorFactory feet1 = new UnitCreatorFactory(0.0, FEET);
+        UnitCreatorFactory feet2 = new UnitCreatorFactory(1.0, FEET);
         Assert.assertEquals(feet1.getClass(), feet2.getClass());
     }
 }
