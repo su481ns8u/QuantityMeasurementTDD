f7c52ddfcaae9bbb85a2d20b295a19944ede3b6a
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-14 02:31:57 +0530

    ADDED Test to get the same value in both feet and inch

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -39,4 +39,48 @@ public class QuantityMeasurementTest {
         UnitCreatorFactory feet2 = new UnitCreatorFactory(1.0, FEET);
         Assert.assertEquals(feet1.getClass(), feet2.getClass());
     }
+
+    @Test
+    public void givenSameUnitInch_ReturnsTrue() throws QuantityMeasurementException {
+        UnitCreatorFactory inch1 = new UnitCreatorFactory(0.0, INCH);
+        UnitCreatorFactory inch2 = new UnitCreatorFactory(0.0, INCH);
+        Assert.assertTrue(unitComparator.compare(inch1, inch2));
+    }
+
+    @Test
+    public void givenNullValueWhileComparingUnitInch_ShouldReturnFalse() {
+        UnitCreatorFactory inch1 = new UnitCreatorFactory(0.0, INCH);
+        try {
+            unitComparator.compare(inch1, null);
+        } catch (QuantityMeasurementException e) {
+            Assert.assertEquals(NO_UNIT_FOUND, e.type);
+        }
+    }
+
+    @Test
+    public void givenSameReferanceOfUnitInch_ShouldReturnTrue() {
+        UnitCreatorFactory inch = new UnitCreatorFactory(0.0, INCH);
+        Assert.assertEquals(inch, inch);
+    }
+
+    @Test
+    public void givenTwoObjectsOfInchWithSameType_ShouldReturnTrue() {
+        UnitCreatorFactory inch1 = new UnitCreatorFactory(0.0, INCH);
+        UnitCreatorFactory inch2 = new UnitCreatorFactory(1.0, INCH);
+        Assert.assertEquals(inch2.getClass(), inch1.getClass());
+    }
+
+    @Test
+    public void givenTwoUnitObjects_WithSameLengthButDifferentUnits_ShouldReturnTrue() throws QuantityMeasurementException {
+        UnitCreatorFactory feet = new UnitCreatorFactory(0.0, FEET);
+        UnitCreatorFactory inch = new UnitCreatorFactory(0.0, INCH);
+        Assert.assertTrue(unitComparator.compare(feet,inch));
+    }
+
+    @Test
+    public void givenTwoUnitObjects_WithNon0SameLengthButDifferentUnits_ShouldReturnTrue() throws QuantityMeasurementException {
+        UnitCreatorFactory feet = new UnitCreatorFactory(1.0, FEET);
+        UnitCreatorFactory inch = new UnitCreatorFactory(12.0, INCH);
+        Assert.assertTrue(unitComparator.compare(feet,inch));
+    }
 }
