234359db8609c4affd4e4b5d9952fcfc6ad28527
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-15 23:47:28 +0530

    ADDED Class to create volume units

--- src/main/java/com/quantityMeasurement/Factories/VolumeUnitCreatorFactory.java
+++ src/main/java/com/quantityMeasurement/Factories/VolumeUnitCreatorFactory.java
@@ -4,9 +4,11 @@ import com.quantityMeasurement.Exceptions.QuantityMeasurementException;
 import com.quantityMeasurement.Utilities.Unit;
 import com.quantityMeasurement.Utilities.Units;
 
+import java.util.Objects;
+
 import static com.quantityMeasurement.Exceptions.QuantityMeasurementException.ExceptionType.NEGATIVE_VALUE;
 
-public class LengthUnitCreatorFactory implements Units {
+public class VolumeUnitCreatorFactory implements Units {
     private Unit unit;
     private double value;
 
@@ -17,7 +19,7 @@ public class LengthUnitCreatorFactory implements Units {
      * @param unit
      * @throws QuantityMeasurementException
      */
-    public LengthUnitCreatorFactory(double value, Unit unit) throws QuantityMeasurementException {
+    public VolumeUnitCreatorFactory(double value, Unit unit) throws QuantityMeasurementException {
         if (value < 0) throw new QuantityMeasurementException(NEGATIVE_VALUE);
 //        if (unit != INCH || unit != FEET || unit != CM || unit != YARD)
 //            throw new QuantityMeasurementException(UNIT_NOT_ACCEPTED);
@@ -26,32 +28,30 @@ public class LengthUnitCreatorFactory implements Units {
     }
 
     @Override
-    public void convert(Unit unit) {
-        this.value = value / unit.conversionFactor;
-        this.unit = unit;
+    public double getValue() {
+        return 0;
     }
 
     @Override
     public Unit getUnit() {
-        return unit;
+        return null;
     }
 
     @Override
-    public double getValue() {
-        return value;
+    public void convert(Unit unit) {
+
     }
 
-    /**
-     * Method to check equality of two objects
-     *
-     * @param o
-     * @return
-     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        LengthUnitCreatorFactory that = (LengthUnitCreatorFactory) o;
+        VolumeUnitCreatorFactory that = (VolumeUnitCreatorFactory) o;
         return Double.compare(that.value, value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(unit, value);
     }
 }
