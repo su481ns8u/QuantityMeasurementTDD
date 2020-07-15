e984eb67741ec539ab82c36056899b1b0c96abad
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-16 00:28:25 +0530

    REFACTORED Unit enum to create units for weight

--- src/main/java/com/quantityMeasurement/Utilities/Unit.java
+++ src/main/java/com/quantityMeasurement/Utilities/Unit.java
@@ -8,7 +8,11 @@ public enum Unit {
 
     LITRE(1),
     GALLON(3.78),
-    MILLI_LITER(1.0 / 1000.0);
+    MILLI_LITER(1.0 / 1000.0),
+
+    KG(1),
+    GRAM(1.0 / 1000.0),
+    TONS(1000);
 
     public double conversionFactor;
 
